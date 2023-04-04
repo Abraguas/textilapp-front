07cd7898b82e44aae278dd34cf11bbdffcd24070
Abraguas <fmoises2002@gmail.com>
2023-04-04 16:53:15 -0300

    Create Product model

---
+++ src/app/models/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+    id: number;
+    name: string;
+    description: string;
+    brand: any;
+    pricePerUnit: number;
+    unit: any;
+    observations: string;
+    stock: number;
+    measurment: string;
+    color: any;
+    subCategory: any;
+    image: string;
+}
