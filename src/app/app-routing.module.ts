8bef303b5800bb1047e0d2ec0416da2bb85b4143
Abraguas <fmoises2002@gmail.com>
2023-04-04 16:50:02 -0300

    Add home and store path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreSidebarComponent } from './components/bars/store-sidebar/store-sidebar.component';
+import { HomeComponent } from './components/home/home.component';
+import { TestComponent } from './components/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { 
+    path: 'store', component: StoreSidebarComponent,
+    children: [{path: ':name', component: TestComponent}]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
