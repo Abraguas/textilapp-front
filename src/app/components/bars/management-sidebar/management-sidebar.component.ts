682dc059f21d4fc5a65bd969969d6883036c98cb
Abraguas <fmoises2002@gmail.com>
2023-04-04 16:51:29 -0300

    Create ManagementSidebarComponent

---
+++ src/app/components/bars/management-sidebar/management-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-management-sidebar',
+  templateUrl: './management-sidebar.component.html',
+  styleUrls: ['./management-sidebar.component.css']
+})
+export class ManagementSidebarComponent {
+
+}
