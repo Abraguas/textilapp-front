17d42039468f5852523461a97f08e6e0f4aa4f36
Abraguas <fmoises2002@gmail.com>
2023-04-11 20:10:17 -0300

    Add auth token to CategoryService.getAll()

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CategoryDTO } from '../models/category-dto';
 
@@ -12,6 +12,15 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<CategoryDTO[]>{
-    return this.http.get<CategoryDTO[]>(this.API_URL);
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+
+      'Content-Type': 'application/json',
+
+      'Authorization': `Bearer ${auth_token}`
+
+    });
+    const requestOptions = { headers: headers };
+    return this.http.get<CategoryDTO[]>(this.API_URL, requestOptions);
   }
 }
