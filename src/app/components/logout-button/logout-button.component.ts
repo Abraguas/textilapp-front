f855d0c02f27b007bb2216f26d3c1b3c8530cf22
Abraguas <fmoises2002@gmail.com>
2023-04-11 20:01:38 -0300

    Create logout button

---
+++ src/app/components/logout-button/logout-button.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
+import { SweetAlert } from 'sweetalert/typings/core';
+declare var require: any
+const swal: SweetAlert = require('sweetalert');
+
+@Component({
+  selector: 'app-logout-button',
+  templateUrl: './logout-button.component.html',
+  styleUrls: ['./logout-button.component.css']
+})
+export class LogoutButtonComponent {
+
+  constructor(private router: Router, private sessionService: SessionService) { }
+
+  logout(): void {
+    swal({
+      title: "Cerrar sesión",
+      text: "¿Seguro que quiere cerrar la sesión?",
+      icon: "warning",
+      dangerMode: true,
+      buttons: {
+        cancel: true,
+        confirm: true,
+      }
+    })
+    .then((cerrarSesion: boolean) => {
+      if (cerrarSesion) {
+        localStorage.removeItem('token');
+        this.sessionService.changeState(false);
+        this.router.navigate(['home']);
+        swal("Hasta pronto!", {
+          icon: "success",
+        });
+      }
+    });
+  }
+}
