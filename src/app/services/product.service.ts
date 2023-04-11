9a141a3a094d19996dd3dea98654d75ba567d31d
Abraguas <fmoises2002@gmail.com>
2023-04-11 20:10:39 -0300

    Create product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ProductService {
+
+   private API_URL: string = 'http://localhost:9090/product/';
+
+   constructor(private http: HttpClient) { }
+ 
+   
+   getAll(): Observable<Product[]>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+
+      'Content-Type': 'application/json',
+
+      'Authorization': `Bearer ${auth_token}`
+
+    });
+    const requestOptions = { headers: headers };
+     return this.http.get<Product[]>(this.API_URL + 'all',requestOptions);
+   }
+   getBySubcategory(subCategoryId: number): Observable<Product[]>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+
+      'Content-Type': 'application/json',
+
+      'Authorization': `Bearer ${auth_token}`
+
+    });
+    const requestOptions = { headers: headers };
+    return this.http.get<Product[]>(this.API_URL+subCategoryId,requestOptions);
+  }
+}
