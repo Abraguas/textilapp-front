ace3a91bec2d7b72687ae3d1b1b2c7eb20049cc8
Abraguas <fmoises2002@gmail.com>
2023-04-11 20:03:40 -0300

    Add login and store routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreSidebarComponent } from './components/bars/store-sidebar/store-sidebar.component';
 import { HomeComponent } from './components/home/home.component';
-import { TestComponent } from './components/test/test.component';
+import { ProductCatalogComponent } from './components/product/product-catalog/product-catalog.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginPageComponent},
   { 
     path: 'store', component: StoreSidebarComponent,
-    children: [{path: ':name', component: TestComponent}]
+    children: [
+      {path: ':subCategory', component: ProductCatalogComponent},
+      {path: '', redirectTo: '0',pathMatch: 'full'}
+    ]
   },
 ];
 
