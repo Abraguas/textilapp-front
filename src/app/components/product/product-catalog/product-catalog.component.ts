e846a21d6a66bbfe9d1fdd28bd411112c34e64cd
Abraguas <fmoises2002@gmail.com>
2023-04-13 23:14:31 -0300

    Add errors for forbidden and unauthorized requests

--- src/app/components/product/product-catalog/product-catalog.component.ts
+++ src/app/components/product/product-catalog/product-catalog.component.ts
@@ -7,6 +7,7 @@ import { OrderDTO } from 'src/app/models/order-dto';
 import { Product } from 'src/app/models/product';
 import { OrderService } from 'src/app/services/order.service';
 import { ProductService } from 'src/app/services/product.service';
+import { SessionService } from 'src/app/services/session.service';
 import { SweetAlert } from 'sweetalert/typings/core';
 declare var require: any
 const swal: SweetAlert = require('sweetalert');
@@ -17,8 +18,8 @@ const swal: SweetAlert = require('sweetalert');
   styleUrls: ['./product-catalog.component.css']
 })
 export class ProductCatalogComponent implements OnInit, OnDestroy {
-  totalPrice: number;
-  totalQuantity: number;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
   subCategory: number;
   products: Product[];
   subscription: Subscription;
@@ -29,7 +30,8 @@ export class ProductCatalogComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private router: Router,
     private productService: ProductService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private sessionService: SessionService
   ) { }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
@@ -71,6 +73,18 @@ export class ProductCatalogComponent implements OnInit, OnDestroy {
             return;
           },
           error: (e) => {
+            if (e.status === 403) {
+              swal({ title: 'Error!', text: 'No tienes permitido el acceso a esta pagina!', icon: 'error' }).then(() => {
+                this.router.navigate(['home']);
+                return;
+              });
+            }
+            if (e.status === 401) {
+              swal({ title: 'Tu sesión ha expirado!', text: '', icon: 'error' }).then(() => {
+                this.sessionService.logout();
+                return;
+              });
+            }
             console.error(e);
             return;
           }
@@ -117,12 +131,27 @@ export class ProductCatalogComponent implements OnInit, OnDestroy {
     this.subscription.add(
       this.orderService.save(this.order).subscribe({
         next: () => {
-          swal({ title: 'Listo!', text: 'Registraste tu pedido con éxito.', icon: 'success' });
-          this.router.navigate(['/home']);
+          swal({ title: 'Listo!', text: 'Registraste tu pedido con éxito.', icon: 'success' }).then(() => {
+            this.router.navigate(['home']);
+          });
         },
         error: (e) => {
-          swal({ title: 'Error!', text: 'Ocurrió un error', icon: 'error' });
-          console.error(e);
+          if (e.status === 403) {
+            swal({ title: 'Error!', text: 'No tienes permitido el acceso a esta pagina!', icon: 'error' }).then(() => {
+              this.router.navigate(['home']);
+              return;
+            });
+          }
+          if (e.status === 401) {
+            swal({ title: 'Error!', text: 'Tu sesión ha expirado!', icon: 'error' }).then(() => {
+              this.sessionService.logout();
+              return;
+            });
+          }
+          else {
+            swal({ title: 'Error!', text: 'Ocurrió un error', icon: 'error' });
+            console.error(e);
+          }
         }
       })
     )
