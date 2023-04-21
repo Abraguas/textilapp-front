33dfd39ecab588a4e45c71795f042f7f2a6311aa
Abraguas <fmoises2002@gmail.com>
2023-04-21 10:26:56 -0300

    Create aux service for brand, color and unit GET

---
+++ src/app/services/auxiliar.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Unit } from '../models/unit';
+import { Color } from '../models/color';
+import { Brand } from '../models/brand';
+
+@Injectable()
+export class AuxiliarService {
+
+    private API_URL: string = 'http://localhost:9090/';
+
+    constructor(private http: HttpClient) { }
+
+    getUnits(): Observable<Unit[]> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.get<Unit[]>(this.API_URL + 'unit', requestOptions);
+    }
+    getColors(): Observable<Color[]> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.get<Color[]>(this.API_URL + 'color', requestOptions);
+    }
+    getBrands(): Observable<Brand[]> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.get<Brand[]>(this.API_URL + 'brand', requestOptions);
+    }
+}
