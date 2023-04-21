ca839f35db50f5e17cbd65a891f78e38c1d252cd
Abraguas <fmoises2002@gmail.com>
2023-04-21 10:28:04 -0300

    Implement list, unlist, save, getlisted, update

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,33 +6,104 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ProductService {
 
-   private API_URL: string = 'http://localhost:9090/product/listed';
+    private API_URL: string = 'http://localhost:9090/product';
 
-   constructor(private http: HttpClient) { }
- 
-   
-   getAll(): Observable<Product[]>{
-    let auth_token = localStorage.getItem('token');
-    const headers = new HttpHeaders({
+    constructor(private http: HttpClient) { }
 
-      'Content-Type': 'application/json',
+    getAll(): Observable<Product[]> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
 
-      'Authorization': `Bearer ${auth_token}`
+            'Content-Type': 'application/json',
 
-    });
-    const requestOptions = { headers: headers };
-     return this.http.get<Product[]>(this.API_URL,requestOptions);
-   }
-   getBySubcategory(subCategoryId: number): Observable<Product[]>{
-    let auth_token = localStorage.getItem('token');
-    const headers = new HttpHeaders({
+            'Authorization': `Bearer ${auth_token}`
 
-      'Content-Type': 'application/json',
+        });
+        const requestOptions = { headers: headers };
+        return this.http.get<Product[]>(this.API_URL + '/all', requestOptions);
+    }
+    getById(productId: number): Observable<Product> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
 
-      'Authorization': `Bearer ${auth_token}`
+            'Content-Type': 'application/json',
 
-    });
-    const requestOptions = { headers: headers };
-    return this.http.get<Product[]>(this.API_URL+'/'+subCategoryId,requestOptions);
-  }
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.get<Product>(`${this.API_URL}/${productId}`, requestOptions);
+    }
+    getAllListed(): Observable<Product[]> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.get<Product[]>(this.API_URL + '/listed', requestOptions);
+    }
+    getListedBySubcategory(subCategoryId: number): Observable<Product[]> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.get<Product[]>(this.API_URL + '/listed/' + subCategoryId, requestOptions);
+    }
+    save(product: Product): Observable<any> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.post<any>(this.API_URL, product, requestOptions);
+    }
+    update(product: Product, productId: number): Observable<any> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.put<any>(`${this.API_URL}/${productId}`, product, requestOptions);
+    }
+    list(productId: number): Observable<any> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.put<any>(`${this.API_URL}/list/${productId}`, {}, requestOptions);
+    }
+    unlist(productId: number): Observable<any> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.put<any>(`${this.API_URL}/unlist/${productId}`, {}, requestOptions);
+    }
 }
