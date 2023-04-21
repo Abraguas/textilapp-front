5c75bf0510b3e5a9f9648496493ecc65b1e28127
Abraguas <fmoises2002@gmail.com>
2023-04-21 10:31:05 -0300

    Reformat image and description

--- src/app/components/product/product-button/product-button.component.ts
+++ src/app/components/product/product-button/product-button.component.ts
@@ -3,50 +3,53 @@ import { OrderDetailDTO } from 'src/app/models/order-detail-dto';
 import { Product } from 'src/app/models/product';
 
 @Component({
-  selector: 'app-product-button',
-  templateUrl: './product-button.component.html',
-  styleUrls: ['./product-button.component.css']
+    selector: 'app-product-button',
+    templateUrl: './product-button.component.html',
+    styleUrls: ['./product-button.component.css']
 })
 export class ProductButtonComponent {
-  @Input() modalId: number = 1;
-  @Input() disabled: boolean;
-  @Input() product: Product;
-  @Output() onAdd = new EventEmitter<OrderDetailDTO>();
+    @Input() isNonFunctional = false;
+    @Input() modalId: number = 1;
+    @Input() disabled: boolean;
+    @Input() product: Product;
+    @Output() onAdd = new EventEmitter<OrderDetailDTO>();
 
-  orderDetail: OrderDetailDTO;
-  defaultRoute:string = "../../assets/img/default-item.jpg";
-  quantity: number = 1;
-  maxQuantity: number = 20;
-  minQuantity: number = 1;
+    orderDetail: OrderDetailDTO;
+    defaultRoute: string = "../../assets/img/default-item.jpg";
+    quantity: number = 1;
+    maxQuantity: number = 20;
+    minQuantity: number = 1;
+    onImgError(event: any): void {
+        event.target.src = this.defaultRoute;
+    }
+    quantityChange(x: boolean): void {
+        if (x) {
+            if (this.quantity >= this.maxQuantity) {
+                return
+            }
+            this.quantity = this.quantity + 1;
+        }
+        else {
+            if (this.quantity <= this.minQuantity) {
+                return
+            }
+            this.quantity = this.quantity - 1;
 
-  quantityChange(x: boolean): void {
-    if(x) {
-      if(this.quantity >= this.maxQuantity){
-        return
-      }
-      this.quantity=this.quantity+1;
+        }
     }
-    else {
-      if(this.quantity <= this.minQuantity){
-        return
-      }
-      this.quantity=this.quantity-1;
-      
+    add(): void {
+        this.orderDetail = {
+            quantity: this.quantity,
+            pricePerUnit: this.product.pricePerUnit,
+            product: this.product
+        }
+        this.onAdd.emit(this.orderDetail);
+        this.disabled = true;
+        this.clearModal();
     }
-  }
-  add(): void {
-    this.orderDetail = {
-      quantity: this.quantity,
-      pricePerUnit: this.product.pricePerUnit,
-      product: this.product
-    }
-    this.onAdd.emit(this.orderDetail);
-    this.disabled = true;
-    this.clearModal();
-  }
 
-  clearModal(): void {
-    this.orderDetail = new OrderDetailDTO();
-    this.quantity = 1;
-  }
+    clearModal(): void {
+        this.orderDetail = new OrderDetailDTO();
+        this.quantity = 1;
+    }
 }
