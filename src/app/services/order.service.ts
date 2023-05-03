ca3ac91d7faf79d74ba1ced9267aaa04c9c3e143
Abraguas <fmoises2002@gmail.com>
2023-05-02 21:20:41 -0300

    Implement orderService getMyOrders and cancelOrder

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,22 +2,47 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { OrderDTO } from '../models/order-dto';
 import { Observable } from 'rxjs';
+import { GetOrderDTO } from '../models/get-order-dto';
 
 @Injectable()
 export class OrderService {
-  private API_URL: string = 'http://localhost:9090/order';
+    private API_URL: string = 'http://localhost:9090/order';
 
-  constructor(private http: HttpClient) { }
-  save(orderDTO: OrderDTO): Observable<any> {
-    let auth_token = localStorage.getItem('token');
-    const headers = new HttpHeaders({
+    constructor(private http: HttpClient) { }
+    save(orderDTO: OrderDTO): Observable<any> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
 
-      'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
 
-      'Authorization': `Bearer ${auth_token}`
+            'Authorization': `Bearer ${auth_token}`
 
-    });
-    const requestOptions = { headers: headers };
-    return this.http.post<any>(this.API_URL, orderDTO, requestOptions);
-  }
+        });
+        const requestOptions = { headers: headers };
+        return this.http.post<any>(this.API_URL, orderDTO, requestOptions);
+    }
+    getMyOrders(): Observable<GetOrderDTO[]> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.get<GetOrderDTO[]>(this.API_URL + '/myOrders', requestOptions);
+    }
+    cancelOrder(orderId: number): Observable<any> {
+        let auth_token = localStorage.getItem('token');
+        const headers = new HttpHeaders({
+
+            'Content-Type': 'application/json',
+
+            'Authorization': `Bearer ${auth_token}`
+
+        });
+        const requestOptions = { headers: headers };
+        return this.http.put<any>(`${this.API_URL}/cancel/${orderId}`, {}, requestOptions);
+    }
 }
