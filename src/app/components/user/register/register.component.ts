31be17bd294edfcba313e4a04a81826d9bd3cb09
Abraguas <fmoises2002@gmail.com>
2023-06-04 16:18:33 -0300

    Add terms and condition checkbox

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -9,63 +9,72 @@ declare var require: any
 const swal: SweetAlert = require('sweetalert');
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
 
-  form: FormGroup;
-  private subscription = new Subscription();
+    form: FormGroup;
+    private subscription = new Subscription();
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private userService: UserService,
-    private router: Router
-  ) {
-    this.form = this.formBuilder.group(
-      {
-        username: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-        password: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-        name: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-        lastname: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-        phonenumber: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-        email: [, [Validators.required, Validators.email]]
-      }
-    );
-  }
-
-  ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  register(): void {
-    if (this.form.valid) {
-      let body: ClientRegisterDTO = this.form.value as ClientRegisterDTO;
-      this.subscription.add(
-        this.userService.registerClient(body).subscribe({
-          next: (r) => {
-            swal({ title: 'Listo!', text: `Usuario registrado exitosamente. Proceda a iniciar sesión con las mismas credenciales.`, icon: 'success' }).then(() =>{
-              this.router.navigate(['login']);
-            });
-            
-          },
-          error: (e) => {
-            console.log(e);
-            swal({ title: 'Error!', text: `Error al registrarse: ${e.error.message}`, icon: 'error' });
-          }
-        })
-      );
+    constructor(
+        private formBuilder: FormBuilder,
+        private userService: UserService,
+        private router: Router
+    ) {
+        this.form = this.formBuilder.group(
+            {
+                username: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+                password: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+                name: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+                lastname: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+                phonenumber: [, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+                email: [, [Validators.required, Validators.email]],
+                termsCheck: [false, [Validators.requiredTrue]]
+            }
+        );
     }
-    else {
-      swal({ title: 'Atención!', text: `Revise y complete todos los campos!`, icon: 'warning' });
-    }
-  }
 
-  return(): void {
-    this.router.navigate(['home']);
-  }
+    ngOnInit(): void {
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+    register(): void {
+        if (this.form.valid) {
+            let body: ClientRegisterDTO = this.form.value as ClientRegisterDTO;
+            this.subscription.add(
+                this.userService.registerClient(body).subscribe({
+                    next: (r) => {
+                        swal({ title: 'Listo!', text: `Usuario registrado exitosamente. Proceda a iniciar sesión con las mismas credenciales.`, icon: 'success' }).then(() => {
+                            this.router.navigate(['login']);
+                        });
+
+                    },
+                    error: (e) => {
+                        console.log(e);
+                        let errorString;
+                        if (e.error.message.toLowerCase().includes('email')) {
+                            errorString = 'El email ya se encuentra en uso';
+                        } else if (e.error.message.toLowerCase().includes('username')) {
+                            errorString = 'El usuario ya se encuentra en uso';
+                        } else {
+                            errorString = e.error.message;
+                        }
+                        swal({ title: 'Error!', text: `Error al registrarse: ${errorString}`, icon: 'error' });
+                    }
+                })
+            );
+        }
+        else {
+            swal({ title: 'Atención!', text: `Revise y complete todos los campos!`, icon: 'warning' });
+        }
+    }
+
+    return(): void {
+        this.router.navigate(['home']);
+    }
 }
